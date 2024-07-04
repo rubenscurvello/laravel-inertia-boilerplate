47f72bb0d4faeebdd12e783b77c210d08d8b4f3f
Anwar Jahid <ajr.jahid@gmail.com>
2024-07-04 13:19:17 +0600

    init Meilisearch updated

--- app/Console/Commands/InitMeilisearch.php
+++ app/Console/Commands/InitMeilisearch.php
@@ -2,8 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Admission;
 use App\Models\MadrasahResult;
 use App\Models\Student;
+use App\Models\SupportConversationMessage;
+use App\Models\User;
 use Illuminate\Console\Command;
 
 class InitMeilisearch extends Command
@@ -13,14 +16,14 @@ class InitMeilisearch extends Command
      *
      * @var string
      */
-    protected $signature =  'init:meilisearch';
+    protected $signature = 'meilisearch:init';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description =  'Inits Meilisearch indexes data';
+    protected $description = 'Inits Meilisearch indexes data';
 
     /**
      * Create a new command instance.
@@ -39,7 +42,10 @@ class InitMeilisearch extends Command
      */
     public function handle()
     {
-        MadrasahResult::makeAllSearchable();
         Student::makeAllSearchable();
+        MadrasahResult::makeAllSearchable();
+        SupportConversationMessage::makeAllSearchable();
+        Admission::makeAllSearchable();
+        User::makeAllSearchable();
     }
 }
